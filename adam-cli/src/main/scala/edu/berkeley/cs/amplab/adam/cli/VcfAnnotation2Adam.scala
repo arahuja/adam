a90efebb8516776f9b01dd4fb90f819e1bfec600
arahuja <aahuja11@gmail.com>
2014-02-07 17:54:02 -0500

    init anno

---
+++ adam-cli/src/main/scala/edu/berkeley/cs/amplab/adam/cli/VcfAnnotation2Adam.scala
@@ -0,0 +1,12 @@
+package edu.berkeley.cs.amplab.adam.cli
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arahuja
+ * Date: 2/6/14
+ * Time: 4:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class VcfAnnotation2Adam {
+
+}
